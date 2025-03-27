8dc9ee4570e9452cf5ae85de1cecfdfe275160ad
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-03-28 08:07:35 +1100

    Change error msg position on input field

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -5,17 +5,19 @@ const InputField = ({label, inputFieldAttribute, invalid=false, errorMessage=""}
         <Form.Group className="my-3">
             <Form.Label className="fw-bold">
                 {label}
-                {errorMessage && 
-                    <span style={{fontSize: "14px"}}className="text-danger ps-4">
-                        * {errorMessage}
-                    </span>
-                }
             </Form.Label>
-
+        
             <input 
                 {...inputFieldAttribute}
                 className={`${invalid?"input-field-invalid":"input-field"} px-3`}
             />
+
+            {errorMessage && 
+                <p style={{fontSize: "14px"}} className="text-danger fw-bold mt-2">
+                    * {errorMessage}
+                </p>
+            }
+
         </Form.Group>
     )
 }
